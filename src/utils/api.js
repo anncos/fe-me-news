0dcaf2e940d003c8d5c46d88ff194c497294bd3f
Anna-Lena Costermans <anna-lena.costermans@nordnet.se>
2018-05-17 13:35:18 +0200

    Remove comments and obsolete file

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,18 +2,13 @@
 const fetchJson = url => fetch(url).then(res => res.json());
 const first10 = arr => arr.slice(0, 10);
 
-// Items Ids Endpoint https://hacker-news.firebaseio.com/v0/topstories.json
 const getItems = () => {
   return fetchJson('https://hacker-news.firebaseio.com/v0/topstories.json')
     .then(res => { return first10(res)});
-  
-  // TODO: fetch the endpoint, get first 10, return it
 };
 
-// Item endpoint https://hacker-news.firebaseio.com/v0/item/8863.json
 const getItem = (id) => {
   return fetchJson(`https://hacker-news.firebaseio.com/v0/item/${id}.json`);
-  // TODO: fetch relevant item, return it
 }
 
 export const api = {
