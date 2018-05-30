f73ce517c4a279b838ad6f4bc9dd7462485b4136
Anna-Lena Costermans <anna-lena.costermans@nordnet.se>
2018-05-30 13:29:10 +0200

    Create store

--- src/index.js
+++ src/index.js
@@ -4,5 +4,15 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { rootReducer } from "./reducers";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
