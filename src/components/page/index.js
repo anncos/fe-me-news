f65fdccff195697d0fa423dd7880a8b8e6cf8144
Anna-Lena Costermans <anna-lena.costermans@nordnet.se>
2018-06-07 13:46:47 +0200

    Add selectors

--- src/components/page/index.js
+++ src/components/page/index.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import './styles.css';
+import React from "react";
+import { connect } from "react-redux";
+import * as ducks from "../../ducks";
+import "./styles.css";
 
 export const Page = ({ children, isDarkTheme }) => {
-  const className = isDarkTheme ? 'page page-dark' : 'page';
-  return (
-    <div className={ className }>
-      { children }
-    </div>
-  )
-}
+  const className = isDarkTheme ? "page page-dark" : "page";
+  return <div className={className}>{children}</div>;
+};
 
 const mapStateToProps = state => {
   return {
-    isDarkTheme: state.ui.isDarkTheme,
-  }
-}
+    //isDarkTheme: state.ui.isDarkTheme;
+    isDarkTheme: ducks.ui.selectors.isDarkTheme(state)
+  };
+};
 
 export default connect(mapStateToProps, null)(Page);
